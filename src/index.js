8a8ee96bfcd38bbda0454f3d49381f72c785ffef
dcronin47 <dcronin47@gmail.com>
2021-04-21 17:27:50 -0400

    added desk, fixed saving

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 let argumentID = 2;
 let argumentCount = 1
 let id;
+let pageLength;
+let style;
 let arguments = [];
+let evidence = [];
+let associations = [];
 let thesis;
+let evidenceID = 2;
+let evidenceCount = 1;
+let eaID = 2;
+let eaCount = 1;
 
 function addArgument() {
     id = argumentID.toString();
@@ -26,6 +34,58 @@ function removeArgument(argument) {
     console.log("remove argument")
 }
 
+function addEvidence() {
+    id = evidenceID.toString();
+    $("#evidence").append(`
+        <br>
+        <div class="columns" id=${evidenceID}>
+            <div class="column has-text-centered is-three-quarters">
+                <textarea class="textarea evidence" placeholder="e.g. Hello world"></textarea>
+            </div>
+            <div class="select">
+                <select id='topic'>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                </select>
+            </div></div>
+            <div>
+                <input class="button is-danger is-light" value="remove this evidence" id="removeEvidence" onclick="removeEvidence(${id})"/>
+                </div>
+            </div>
+    `)
+    evidenceID++; evidenceCount++;
+    console.log("add argument" + argumentCount);
+}
+
+function removeEvidence(evidence) {
+    document.getElementById(evidence.toString()).remove();
+    evidenceCount--;
+    console.log("remove evidence")
+}
+
+// function addEvidenceArgument() {
+//     id = eaID.toString();
+//     $("#ea").append(`
+//         <div class="columns" id=${eaID}>
+//             <div class="column has-text-centered is-three-quarters">
+//                 <textarea class="textarea evidence" placeholder="e.g. Hello world"></textarea>
+//             </div>
+//             <div class="column">
+//                 <input class="button is-danger is-light" value="remove this argument" id="removeEa" onclick="removeEvidenceArgument(${id})"/>
+//                 </div>
+//             </div>
+//     `)
+//     eaID++; eaCount++;
+//     console.log("add ea" + eaCount);
+// }
+
+// function removeEvidenceArgument(ea) {
+//     document.getElementById(ea.toString()).remove();
+//     eaCount--;
+//     console.log("remove ea")
+// }
+
 // save functions save user's state when they navigate off a page
 
 function saveThesis() {
@@ -34,14 +94,15 @@ function saveThesis() {
 }
 
 function saveLayout() {
-
+    localStorage.setItem('length', document.getElementById("pageLength").value)
+    localStorage.setItem('topic', document.getElementById('topic').value)
 }
 
 function saveArguments() {
-    arguments = $('textarea').map(function() {
+    args = $('textarea').map(function() {
         return $.trim(this.value);
    }).get();
-   localStorage.setItem('arguments', arguments)
+   localStorage.setItem('arguments', args)
 }
 
 function saveEvidence() {
@@ -49,6 +110,10 @@ function saveEvidence() {
         return $.trim(this.value);
    }).get();
    localStorage.setItem('evidence', evidence)
+}
+
+function saveDesk() {
+
 }
 
 // load functions load user's data when they return to a page
@@ -72,6 +137,27 @@ function loadArgument() {
 
 function loadEvidence() {
     console.log("load evidence")
+}
+
+function loadDesk() {
+    console.log(localStorage.getItem("arguments"))
+    for (let i=0; i<localStorage.getItem("arguments").length; i++) {
+        $('#desk').append(`
+        <p>argument</p>
+        `)
+    };
+}
+
+function loadOutline() {
+    $("#outline").append(`
+    <p>${localStorage.getItem('thesis')}</p>
+    <p>${localStorage.getItem('arguments')}</p>
+    <p>${localStorage.getItem('evidence')}</p>
+    <p>${localStorage.getItem('length')}</p>
+    <p>${localStorage.getItem('topic')}</p>
+    `
+    )
+    console.log('loading outline')
 }
 
 function updateTextInput(val) {
